b3823ce4ba677cee0af1fd6f0daab91bd581b580
Srishti Mishra <srishtimishra5672@gmail.com>
2023-12-18 13:43:18 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const colors = require('colors');
+const mongoose = require("mongoose");
+const colors = require("colors");
 
 const MONGO_URL = "mongodb://localhost:27017/BloodBank";
 const connnectDB = async () => {
-    try {
-        await mongoose.connect(MONGO_URL);
-        console.log(`Connected to DB  ${mongoose.connection.host}`.bgMagenta.white);
-    } catch( error) {
-        console.log(`Mongo DB error ${error}`.bgRed.white);
-    }
-}
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log(`Connected to DB  ${mongoose.connection.host}`.bgMagenta.white);
+  } catch (error) {
+    console.log(`Mongo DB error ${error}`.bgRed.white);
+  }
+};
 
 module.exports = connnectDB;
